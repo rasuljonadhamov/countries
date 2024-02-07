0186dbd113931a7957dc2e9d9226953be2c0e842
rasuljonadhamov <rasuljonadhamov@gmail.com>
2024-02-07 17:51:35 +0500

    add countries

---
+++ app/components/Countries.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+interface Country {
+  id: string;
+  name: {
+    common: string;
+  };
+  region: string;
+  flags: {
+    png?: string;
+  };
+}
+interface CountriesProps {
+  countries: Country[];
+}
+
+const Countries: React.FC<CountriesProps> = ({ countries }) => {
+  return (
+    <div className="container px-16 mx-auto mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14 cursor-pointer">
+      {countries.map((country) => (
+        <div
+          key={country.id}
+          className="card rounded-lg shadow-md hover:shadow-lg"
+        >
+          <img
+            src={country.flags.png}
+            alt={country.region}
+            className="w-full h-48 object-cover rounded-t-lg"
+          />
+          <div className="p-4">
+            <h1 className=" text-xl font-bold">{country.name.common}</h1>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Countries;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,33 @@
+import Countries from "./components/Countries";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
-export default function Home() {
+async function getData() {
+  try {
+    const res = await fetch(
+      "https://frontend-mentor-apis-6efy.onrender.com/countries"
+    );
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+    const data = await res.json();
+
+    return data.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+}
+
+export default async function Home() {
+  const data = await getData();
+
   return (
     <div className="container h-[100vh] bg-slate-50 ">
       <Header title="Where in the world?" />
       <Hero />
+
+      <Countries countries={data} />
     </div>
   );
 }
